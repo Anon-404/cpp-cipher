211142906edeb2bc334138c4eb4d7c5c298d090e
Anon404 <138273350+Anon-404@users.noreply.github.com>
2024-10-01 21:04:41 +0600

    Create cpp-cipher.cpp

---
+++ cpp-cipher.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	string smg = "fuck you man 🖕🖕";
+	
+	for(char c : smg){
+	    int x = c;
+	    cout<<" "<< x;
+	}
+	
+	return 0;
+	
+}
