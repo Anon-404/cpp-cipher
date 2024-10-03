7c52584d9ce7d7cb33bd6550ba820a9c3679a603
Anon404 <138273350+Anon-404@users.noreply.github.com>
2024-10-03 09:58:09 +0600

    Update cpp-cipher.cpp

--- cpp-cipher.cpp
+++ cpp-cipher.cpp
@@ -2,13 +2,80 @@
 
 using namespace std;
 
+
+
+
+
+void banner(){
+    
+    cout<< "\n\t banner option here\n" <<endl;
+    
+}
+
+
+
+
+
+int encryption(string sentence){
+    
+    //cout << sentence;
+    int shift;
+    cout<< "Enter shift number : ";
+    cin>> shift;
+    for(char c : sentence){
+        int a = c + shift;
+        char b = a;
+        cout<< b;
+    }
+    return 0;
+
+}
+
+
+
+
+
+int decryption(string sentence){
+    
+    
+    cout << sentence;
+    return 0;
+
+}
+
+
+
+
+
 int main(){
 	
-	string smg = "fuck you man 🖕🖕";
+	banner();
 	
-	for(char c : smg){
-	    int x = c;
-	    cout<<" "<< x;
+	cout<< "1) Encryption" <<endl << "2) Decryption" <<endl;
+	int opt;	
+	cout<< "\nOption number : ";
+	cin>> opt;
+	
+	if(opt == 1){
+	    
+	    banner();
+	    
+	    string text;
+	    cout << "Enter your text here : ";
+	    cin >> text;
+	    encryption(text);
+	    
+	}else if(opt == 2){
+	    
+	    banner();
+	    
+	    string text;
+	    cout << "Enter encrypted text here : ";
+	    cin >> text;
+	    decryption(text);
+	    
+	}else{
+	    cout<< "rong option";
 	}
 	
 	return 0;
