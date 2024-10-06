7ed63aec44894f14df462a34811d9afee93dfd8a
Anon404 <138273350+Anon-404@users.noreply.github.com>
2024-10-06 17:57:04 +0600

    Update cpp-cipher.cpp

--- cpp-cipher.cpp
+++ cpp-cipher.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+#define BOLD_BLACK   "\033[1;30m"
+#define BOLD_RED     "\033[1;31m"
+#define BOLD_GREEN   "\033[1;32m"
+#define BOLD_YELLOW  "\033[1;33m"
+#define BOLD_BLUE    "\033[1;34m"
+#define BOLD_MAGENTA "\033[1;35m"
+#define BOLD_CYAN    "\033[1;36m"
+#define BOLD_WHITE   "\033[1;37m"
+#define RESET   "\033[0m"
 
-
-
-
-void banner(){
-    
-    cout<< "\n\t banner option here\n" <<endl;
-    
+void banner() {
+    cout << BOLD_CYAN << "\n\n                               _       __                 \n";
+    cout << "  _________  ____        _____(_)___  / /_  ___  _____    \n";
+    cout << " / ___/ __ \\/ __ \\______/ ___/ / __ \\/ __ \\/ _ \\/ ___/    \n";
+    cout << "/ /__/ /_/ / /_/ /_____/ /__/ / /_/ / / / /  __/ /        \n";
+    cout << "\\___/ .___/ .___/      \\___/_/ .___/_/ /_/\\___/_/         \n";
+    cout << "   /_/   /_/                /_/                            \n\n\n" << RESET;
 }
 
-
-
-
-
-int encryption(string sentence){
+int encryption(string sentence) {
+    int shift;
+    cout << BOLD_WHITE << "Enter shift number: ";
+    cin >> shift;
     
-    //cout << sentence;
+    cout << BOLD_CYAN << "\nCipher text : ";
+
+    for (char c : sentence) {
+        
+        if (c >= 'A' && c <= 'Z') {
+            c = ((c - 'A' + shift) % 26) + 'A';
+            cout << c;
+        }
+        
+        else if (c >= 'a' && c <= 'z') {
+            c = ((c - 'a' + shift) % 26) + 'a';
+            cout << c;
+        }
+        else {
+            cout << c;
+        }
+    }
+    
+    cout << RESET << "\n";
+
+    return 0;
+ 
+}
+
+int decryption(string sentence){
+    
     int shift;
     cout<< "Enter shift number : ";
     cin>> shift;
@@ -27,42 +61,30 @@ int encryption(string sentence){
         char b = a;
         cout<< b;
     }
+    //cout << sentence;
     return 0;
 
 }
-
-
-
-
-
-int decryption(string sentence){
-    
-    
-    cout << sentence;
-    return 0;
-
-}
-
-
-
-
 
 int main(){
 	
 	banner();
 	
-	cout<< "1) Encryption" <<endl << "2) Decryption" <<endl;
+	cout << BOLD_GREEN << "1) Encryption" <<endl << "2) Decryption" << RESET <<endl;
 	int opt;	
-	cout<< "\nOption number : ";
+	cout<< BOLD_WHITE << "\nOption number : ";
 	cin>> opt;
 	
 	if(opt == 1){
 	    
+	    system("clear");
+	    
 	    banner();
 	    
 	    string text;
-	    cout << "Enter your text here : ";
-	    cin >> text;
+	    cout << BOLD_WHITE << "Enter your text here : ";
+	    cin.ignore();
+	    getline(cin,text);
 	    encryption(text);
 	    
 	}else if(opt == 2){
@@ -70,12 +92,12 @@ int main(){
 	    banner();
 	    
 	    string text;
-	    cout << "Enter encrypted text here : ";
+	    cout << BOLD_WHITE << "Enter encrypted text here : ";
 	    cin >> text;
 	    decryption(text);
 	    
 	}else{
-	    cout<< "rong option";
+	    cout << BOLD_RED << "rong option";
 	}
 	
 	return 0;
